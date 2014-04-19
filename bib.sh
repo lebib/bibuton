daad24e4489aa9756411fee52e66fc6f97ff8df8
lk <lk-minot@users.noreply.github.com>
2014-04-20 00:53:27 +0200

    ajout fonction clignotement led

--- bib.sh
+++ bib.sh
@@ -11,11 +11,26 @@ echo "in" > /sys/class/gpio/gpio4/direction
 echo "1" > /sys/class/gpio/gpio2/value
 echo "1" > /sys/class/gpio/gpio3/value
 
+# fonction de clignotement des leds
+cligno () {
+    while true; do
+        echo "1" > /sys/class/gpio/gpio2/value
+    echo "0" > /sys/class/gpio/gpio3/value
+        sleep 1
+        echo "1" > /sys/class/gpio/gpio3/value
+        echo "0" > /sys/class/gpio/gpio2/value
+        sleep 1
+    done
+} 
+
 USER=""
 PASSWORD=""
 
 while true; do
+    # execute a l'appuie du bouton
     if [ $(cat /sys/class/gpio/gpio4/value) -eq 0 ]; then
+    # fonction cligno en background
+        cligno & 
         # Se connecter et enregistrer la page
         curl -c /tmp/cookies -d "name=$USER&pass=$PASSWORD&form_id=user_login&op=Se+connecter" http://lebib.org/user -s
         curl -b /tmp/cookies http://lebib.org --output /tmp/bib.html -s
@@ -23,17 +38,18 @@ while true; do
         token=$(cat /tmp/bib.html | grep -m 1 form_token | awk '{print $4}' | awk -F '\"' '{print $2}')
         id=$(cat /tmp/bib.html | grep -m 1 form_build_id | awk '{print $29}' | awk -F '\"' '{print $2}')
         state=$(cat /tmp/bib.html | grep -m 1 bibopen | awk '{print $9}' | awk -F '\"' '{print $2}')
-        # Si le bib est fermé allumer led orange, si ouvert allumer led blanche
-            if [ -z $state ]; then
-                echo "0" > /sys/class/gpio/gpio2/value
-                echo "1" > /sys/class/gpio/gpio3/value
-            else
-                echo "0" > /sys/class/gpio/gpio3/value
-                echo "1" > /sys/class/gpio/gpio2/value
-            fi
+        # stock l'etat actuel
+        if [ -z $state ]; then
+            state=1
+        else
+            state=0
+        fi
         # changer l'etat
         curl -b /tmp/cookies -d "op=Changer&form_build_id=$id&form_token=$token&form_id=bibstate_toggle_form" http://lebib.org -s
-        if [ $(cat /sys/class/gpio/gpio3/value) -eq 1 ]; then
+        # kill le dernier jobs
+        kill %+
+        # Si le bib est fermé allumer led orange, si ouvert allumer led blanche
+        if [ $state -eq 1 ]; then
             echo "1" > /sys/class/gpio/gpio3/value
             echo "0" > /sys/class/gpio/gpio2/value
         else
